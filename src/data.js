797a64e66fbf68163e14c580be2fda83781c61bb
Enigma <balsa.fedora@yahoo.com>
2019-09-26 04:46:24 +0200

    finalized

--- src/data.js
+++ src/data.js
@@ -6,7 +6,46 @@ export const storeProducts = [
     price: 130,
     company: "GOOGLE",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      `LTE Advanced 
+      With 50% faster peak speeds in more than 450 cities from coast to coast. 
+      World Device
+      Works in over 200 countries depending on your plan.
+      Standby Time - Up to:
+      19 days
+      Usage Time - Up to:
+      24 hrs
+      Camera
+      12.3 MP
+      Front Camera
+      8 MP
+      Width
+      2.74 in
+      Weight
+      5.04 oz
+      Height
+      5.66 in
+      Screen
+      5.0’’ FHD 441ppi AMOLED, 2.5D Gorilla Glass 4 >75% Active Area
+      Battery
+      2770 mAh
+      Operating System
+      Android Nougat 7.1
+      Storage
+      RAM: 4GB LPDDR4, Flash 32GB or 128GB (Actual formatted capacity is less)
+      Colors
+      Quite Black, Very Silver, or Really Blue (Subject to availability)
+      SAR
+      Head: 0.92 W/Kg@1g, Body: 0.58 W/Kg@1g
+      Hearing Aid Compatibility
+      M4/T4
+      Network
+      3x Carrier aggregation; CAT 9 (450 Mbps DL/50 Mpbs); CDMA: BC0/BC1; LTE: B13, B4, B2, B5;
+      Global Network
+      LTE (Global) B3, B7, B20, B28; GSM Quad B5, B8, B3, B2 (850/900/1800/1900Mhz) UMTS Quad B5, B8, B2, B1 (850, 900, 1900, 2100 MHz);
+      Processor
+      Snapdragon 821; MSM8996 pro AB Quad Core 2+2 Kryo 2.15GHz / 1.6GHz
+      HD Voice
+      Experience HD Voice, Video Calling and Simultaneous Voice & Data. Enable Wi-Fi Calling and make calls anywhere you have a Wi-Fi connection.`,
     inCart: false,
     count: 0,
     total: 0
@@ -18,7 +57,31 @@ export const storeProducts = [
     price: 160,
     company: "SAMSUNG",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      `Network 	Technology 	
+      GSM / HSPA / LTE
+      Launch 	Announced 	2016, February
+      Status 	Available. Released 2016, March
+      Body 	Dimensions 	142.4 x 69.6 x 7.9 mm (5.61 x 2.74 x 0.31 in)
+      Weight 	152 g (5.36 oz)
+      Build 	Front/back glass (Gorilla Glass 4), aluminum frame
+      SIM 	Single SIM (Nano-SIM) - G930F
+      Hybrid Dual SIM (Nano-SIM, dual stand-by) - G930FD
+         Samsung Pay (Visa, MasterCard certified)
+      IP68 dust/water proof (up to 1.5m for 30 mins)
+      Display 	Type 	Super AMOLED capacitive touchscreen, 16M colors
+      Size 	5.1 inches, 71.5 cm2 (~72.1% screen-to-body ratio)
+      Resolution 	1440 x 2560 pixels, 16:9 ratio (~577 ppi density)
+      Protection 	Corning Gorilla Glass 4
+         Always-on display
+      Platform 	OS 	Android 6.0 (Marshmallow), upgradable to Android 8.0 (Oreo); TouchWiz UI
+      Chipset 	Exynos 8890 Octa (14 nm)
+      CPU 	Octa-core (4x2.3 GHz Mongoose & 4x1.6 GHz Cortex-A53)
+      GPU 	Mali-T880 MP12
+      Memory 	Card slot 	microSD, up to 256 GB (uses shared SIM slot) - dual SIM model only
+      Internal 	32GB 4GB RAM, 64GB 4GB RAM
+      Main Camera 	Single 	12 MP, f/1.7, 26mm (wide), 1/2.55", 1.4µm, Dual Pixel PDAF, OIS
+      Features 	LED flash, auto-HDR, panorama.
+     `,
     inCart: false,
     count: 0,
     total: 0
@@ -30,7 +93,24 @@ export const storeProducts = [
     price: 158,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      `Network 	Technology 	
+      GSM / HSPA / LTE
+      Launch 	Announced 	2016, April
+      Status 	Available. Released 2016, May
+      Body 	Dimensions 	145.9 x 71.9 x 9 mm (5.74 x 2.83 x 0.35 in)
+      Weight 	161 g (5.68 oz)
+      Build 	Front glass, aluminum body
+      SIM 	Nano-SIM
+      Display 	Type 	Super LCD5 capacitive touchscreen, 16M colors
+      Size 	5.2 inches, 74.5 cm2 (~71.1% screen-to-body ratio)
+      Resolution 	1440 x 2560 pixels, 16:9 ratio (~565 ppi density)
+      Protection 	Corning Gorilla Glass 3
+      Platform 	OS 	Android 6.0.1 (Marshmallow), upgradable to Android 8.0 (Oreo); Sense UI
+      Chipset 	Qualcomm MSM8996 Snapdragon 820 (14 nm)
+      CPU 	Quad-core (2x2.15 GHz Kryo & 2x1.6 GHz Kryo)
+      GPU 	Adreno 530
+      Memory 	Card slot 	microSD, up to 1 TB (dedicated slot)
+      Internal 	32GB 4GB RAM, 64GB 4GB RAM.`,
     inCart: false,
     count: 0,
     total: 0
@@ -42,7 +122,24 @@ export const storeProducts = [
     price: 180,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      `Network 	Technology 	
+      GSM / HSPA / LTE
+      Launch 	Announced 	2016, April
+      Status 	Available. Released 2016, May
+      Body 	Dimensions 	145.9 x 71.9 x 9 mm (5.74 x 2.83 x 0.35 in)
+      Weight 	161 g (5.68 oz)
+      Build 	Front glass, aluminum body
+      SIM 	Nano-SIM
+      Display 	Type 	Super LCD5 capacitive touchscreen, 16M colors
+      Size 	5.2 inches, 74.5 cm2 (~71.1% screen-to-body ratio)
+      Resolution 	1440 x 2560 pixels, 16:9 ratio (~565 ppi density)
+      Protection 	Corning Gorilla Glass 3
+      Platform 	OS 	Android 6.0.1 (Marshmallow), upgradable to Android 8.0 (Oreo); Sense UI
+      Chipset 	Qualcomm MSM8996 Snapdragon 820 (14 nm)
+      CPU 	Quad-core (2x2.15 GHz Kryo & 2x1.6 GHz Kryo)
+      GPU 	Adreno 530
+      Memory 	Card slot 	microSD, up to 1 TB (dedicated slot)
+      Internal 	32GB 4GB RAM, 64GB 4GB RAM.`,
     inCart: false,
     count: 0,
     total: 0
@@ -54,7 +151,22 @@ export const storeProducts = [
     price: 240,
     company: "htc",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      `Network 	Technology 	
+      GSM / HSPA / LTE
+      Launch 	Announced 	2015, July
+      Status 	Available. Released 2015, July
+      Body 	Dimensions 	146.8 x 70.9 x 8.1 mm (5.78 x 2.79 x 0.32 in)
+      Weight 	138.9 g (4.90 oz)
+      SIM 	Nano-SIM
+      Display 	Type 	Capacitive touchscreen, 16M colors
+      Size 	5.0 inches, 68.9 cm2 (~66.2% screen-to-body ratio)
+      Resolution 	720 x 1280 pixels, 16:9 ratio (~294 ppi density)
+      Platform 	OS 	Android 5.1.1 (Lollipop), upgradable to 6.0.1 (Marshmallow); Sense UI 7
+      Chipset 	Qualcomm MSM8909 Snapdragon 210 (28 nm)
+      CPU 	Quad-core 1.1 GHz Cortex-A7
+      GPU 	Adreno 304
+      Memory 	Card slot 	microSD, up to 256 GB (dedicated slot)
+      Internal 	8GB 1GB RAM, 8GB 1.5GB RAM (T-Mobile)`,
     inCart: false,
     count: 0,
     total: 0
@@ -66,7 +178,22 @@ export const storeProducts = [
     price: 175,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      `Network 	Technology 	
+      GSM / HSPA / LTE
+      Launch 	Announced 	2015, July
+      Status 	Available. Released 2015, July
+      Body 	Dimensions 	146.8 x 70.9 x 8.1 mm (5.78 x 2.79 x 0.32 in)
+      Weight 	138.9 g (4.90 oz)
+      SIM 	Nano-SIM
+      Display 	Type 	Capacitive touchscreen, 16M colors
+      Size 	5.0 inches, 68.9 cm2 (~66.2% screen-to-body ratio)
+      Resolution 	720 x 1280 pixels, 16:9 ratio (~294 ppi density)
+      Platform 	OS 	Android 5.1.1 (Lollipop), upgradable to 6.0.1 (Marshmallow); Sense UI 7
+      Chipset 	Qualcomm MSM8909 Snapdragon 210 (28 nm)
+      CPU 	Quad-core 1.1 GHz Cortex-A7
+      GPU 	Adreno 304
+      Memory 	Card slot 	microSD, up to 256 GB (dedicated slot)
+      Internal 	8GB 1GB RAM, 8GB 1.5GB RAM (T-Mobile)`,
     inCart: false,
     count: 0,
     total: 0
@@ -78,7 +205,46 @@ export const storeProducts = [
     price: 300,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      `LTE Advanced 
+      With 50% faster peak speeds in more than 450 cities from coast to coast. 
+      World Device
+      Works in over 200 countries depending on your plan.
+      Standby Time - Up to:
+      19 days
+      Usage Time - Up to:
+      24 hrs
+      Camera
+      12.3 MP
+      Front Camera
+      8 MP
+      Width
+      2.74 in
+      Weight
+      5.04 oz
+      Height
+      5.66 in
+      Screen
+      5.0’’ FHD 441ppi AMOLED, 2.5D Gorilla Glass 4 >75% Active Area
+      Battery
+      2770 mAh
+      Operating System
+      Android Nougat 7.1
+      Storage
+      RAM: 4GB LPDDR4, Flash 32GB or 128GB (Actual formatted capacity is less)
+      Colors
+      Quite Black, Very Silver, or Really Blue (Subject to availability)
+      SAR
+      Head: 0.92 W/Kg@1g, Body: 0.58 W/Kg@1g
+      Hearing Aid Compatibility
+      M4/T4
+      Network
+      3x Carrier aggregation; CAT 9 (450 Mbps DL/50 Mpbs); CDMA: BC0/BC1; LTE: B13, B4, B2, B5;
+      Global Network
+      LTE (Global) B3, B7, B20, B28; GSM Quad B5, B8, B3, B2 (850/900/1800/1900Mhz) UMTS Quad B5, B8, B2, B1 (850, 900, 1900, 2100 MHz);
+      Processor
+      Snapdragon 821; MSM8996 pro AB Quad Core 2+2 Kryo 2.15GHz / 1.6GHz
+      HD Voice
+      Experience HD Voice, Video Calling and Simultaneous Voice & Data. Enable Wi-Fi Calling and make calls anywhere you have a Wi-Fi connection.`,
     inCart: false,
     count: 0,
     total: 0
@@ -90,7 +256,46 @@ export const storeProducts = [
     price: 30,
     company: "apple",
     info:
-      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      `LTE Advanced 
+      With 50% faster peak speeds in more than 450 cities from coast to coast. 
+      World Device
+      Works in over 200 countries depending on your plan.
+      Standby Time - Up to:
+      19 days
+      Usage Time - Up to:
+      24 hrs
+      Camera
+      12.3 MP
+      Front Camera
+      8 MP
+      Width
+      2.74 in
+      Weight
+      5.04 oz
+      Height
+      5.66 in
+      Screen
+      5.0’’ FHD 441ppi AMOLED, 2.5D Gorilla Glass 4 >75% Active Area
+      Battery
+      2770 mAh
+      Operating System
+      Android Nougat 7.1
+      Storage
+      RAM: 4GB LPDDR4, Flash 32GB or 128GB (Actual formatted capacity is less)
+      Colors
+      Quite Black, Very Silver, or Really Blue (Subject to availability)
+      SAR
+      Head: 0.92 W/Kg@1g, Body: 0.58 W/Kg@1g
+      Hearing Aid Compatibility
+      M4/T4
+      Network
+      3x Carrier aggregation; CAT 9 (450 Mbps DL/50 Mpbs); CDMA: BC0/BC1; LTE: B13, B4, B2, B5;
+      Global Network
+      LTE (Global) B3, B7, B20, B28; GSM Quad B5, B8, B3, B2 (850/900/1800/1900Mhz) UMTS Quad B5, B8, B2, B1 (850, 900, 1900, 2100 MHz);
+      Processor
+      Snapdragon 821; MSM8996 pro AB Quad Core 2+2 Kryo 2.15GHz / 1.6GHz
+      HD Voice
+      Experience HD Voice, Video Calling and Simultaneous Voice & Data. Enable Wi-Fi Calling and make calls anywhere you have a Wi-Fi connection.`,
     inCart: false,
     count: 0,
     total: 0
@@ -104,7 +309,7 @@ export const detailProduct = {
   price: 10,
   company: "google",
   info:
-    "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    `Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.`,
   inCart: false,
   count: 0,
   total: 0
